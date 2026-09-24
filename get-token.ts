6c3b640f92027750f9f0ae79994ea54fb77f0638
anbuselvan-rajavel <anbuselvan@mahadhi.com>
2026-09-24 20:56:11 +0530

    fixed admin login issue

--- get-token.ts
+++ get-token.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 async function login() {
   const mockResponse = NextResponse.json({ ok: true });
-  const token = await createAndSetAuthSession("testadmin@zopapro.com", mockResponse);
+  const token = await createAndSetAuthSession("admin@zopapro.com", mockResponse);
   console.log("TOKEN:", token);
   process.exit(0);
 }
